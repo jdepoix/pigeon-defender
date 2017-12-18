6ddfc9255edfd9e997b79d4e6b22bb8b0ac8741f
Jonas Depoix <jdepoix@seibert-media.net>
2017-12-18 16:08:07 +0100

    messaging service updated

--- messaging/router.js
+++ messaging/router.js
@@ -1,13 +1,16 @@
 'use strict';
 
-module.exports.hello = (event, context, callback) => {
+module.exports.router = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
+      message: 'Router TEST!!',
       input: event,
     }),
   };
+
+  console.log(event);
+  console.log(context);
 
   callback(null, response);
 
