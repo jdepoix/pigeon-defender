7c8138db4b0aabcb2da0ffb8c93517c72832d85b
Jonas Depoix <jdepoix@seibert-media.net>
2018-01-08 00:43:44 +0100

    circuit breaker logic implemented into routing module

--- messaging/routing/circuit-breaker-event-handler.js
+++ messaging/routing/circuit-breaker-event-handler.js
@@ -5,6 +5,11 @@ const RoutingEventHandler = require('./routing-event-handler');
 
 module.exports = class CircuitBreakerEventHandler extends RoutingEventHandler {
   handle() {
-    
+    return dynamoDb.update({
+      TableName: 'PigeonDefender.Groups',
+      Key: {id: this.messageContext.groupData.id},
+      UpdateExpression: 'set active = :active',
+      ExpressionAttributeValues: {':active': !this.messageContext.groupData.active}
+    }).promise();
   }
 };
