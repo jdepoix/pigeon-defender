7ad42419e50f1bee6dc175e6f74e0fea9d7eb38d
Jonas Depoix <jdepoix@seibert-media.net>
2018-01-26 16:28:47 +0100

    angular material dependency added

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,19 +1,26 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
+import {MatToolbarModule, MatIconModule, MatCardModule} from '@angular/material';
+
 import {AppComponent} from './app.component';
 import {AuthenticationService} from './authentication.service';
 import {DeviceService} from './device.service';
 import {GroupService} from './group.service';
 import {DeviceOverviewComponent} from './device-overview/device-overview.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DeviceOverviewComponent
+    DeviceOverviewComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule
   ],
   providers: [
     AuthenticationService,
