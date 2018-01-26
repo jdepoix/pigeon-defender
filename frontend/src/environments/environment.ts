1b7c06b0ddff406443e1d5067d37d748dca71429
Jonas Depoix <jdepoix@seibert-media.net>
2018-01-26 13:10:01 +0100

    aws settings added to environment

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  aws: {
+    region: 'eu-central-1',
+    cognito: {
+      userPoolId: 'eu-central-1_qN02z196d',
+      appClientId: '3mvs5dprsn31i697ul6usam5sv',
+      identifyPoolId: 'eu-central-1:8b6ce127-c6d1-4a84-9300-0162150f0e50'
+    }
+  }
 };
