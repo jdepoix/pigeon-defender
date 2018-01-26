4262af4d8d7617dc3e3023f96639269368c00dcf
Jonas Depoix <jdepoix@seibert-media.net>
2018-01-26 17:54:32 +0100

    login component added

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,14 +1,15 @@
-import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
-import {MatToolbarModule, MatIconModule, MatCardModule} from '@angular/material';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule} from '@angular/forms';
+import {MatToolbarModule, MatIconModule, MatCardModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatSnackBarModule} from '@angular/material';
 
 import {AppComponent} from './app.component';
 import {AuthenticationService} from './authentication.service';
 import {DeviceService} from './device.service';
 import {GroupService} from './group.service';
 import {DeviceOverviewComponent} from './device-overview/device-overview.component';
-import { LoginComponent } from './login/login.component';
+import {LoginComponent} from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -18,9 +19,16 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    // angular material modules
     MatToolbarModule,
     MatIconModule,
-    MatCardModule
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSnackBarModule
   ],
   providers: [
     AuthenticationService,
---
+++ frontend/src/app/login/login.component.ts
@@ -0,0 +1,42 @@
+import {Component} from '@angular/core';
+import {AuthenticationService} from '../authentication.service';
+import {MatSnackBar} from '@angular/material';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  hidePassword = true;
+  email: string;
+  password: string;
+
+  constructor(private _snackBar: MatSnackBar, public authenticationService: AuthenticationService) {}
+
+  login(): void {
+    this.authenticationService.login(this.email, this.password).then(
+      () => this._showSnackbarMessage('Successfully logged in!')
+    ).catch(
+      (error) => {
+        console.error(error);
+        this._showSnackbarMessage('Could not log in! ' + error.message);
+      }
+    );
+  }
+
+  register(): void {
+    this.authenticationService.registerUser(this.email, this.password).then(
+      () => this._showSnackbarMessage('Successfully registered new User!')
+    ).catch(
+      (error) => {
+        console.error(error);
+        this._showSnackbarMessage('Could not register new User! ' + error.message);
+      }
+    );
+  }
+
+  private _showSnackbarMessage(message: string): void {
+    this._snackBar.open(message, null, {duration: 1000});
+  }
+}
